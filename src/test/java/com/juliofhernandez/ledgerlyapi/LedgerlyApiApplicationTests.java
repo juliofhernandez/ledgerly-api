1f50b7e2efeba85f76436aa9ba481e6bb89c020f
juliofhernandez <juliofernando1c@gmail.com>
2026-05-12 10:21:27 +0200

    Initial commit

---
+++ src/test/java/com/juliofhernandez/ledgerlyapi/LedgerlyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliofhernandez.ledgerlyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerlyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
